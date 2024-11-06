7af50d08daa385c191577761d5abafdb8ad15d2d
gowoobro <kobums@naver.com>
2024-11-06 18:21:40 +0900

    update allowOrigin https

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	app.Use(cors.New(cors.Config{
 		AllowHeaders:     "Origin, Content-Type, Authorization, Accept",
 		AllowCredentials: true,
-		AllowOrigins:     "http://140.82.12.99:9002, http://localhost:9002, http://www.gowoobro.com:9002, http://playlist.gowoobro.com, https://www.gowoobro.com:9002, https://playlist.gowoobro.com",
+		AllowOrigins:     "http://localhost:9002, http://140.82.12.99:9002, http://www.gowoobro.com:9002, http://playlist.gowoobro.com, https://140.82.12.99:9002, https://www.gowoobro.com:9002, https://playlist.gowoobro.com",
 		AllowMethods: strings.Join([]string{
 			fiber.MethodGet,
 			fiber.MethodPost,
